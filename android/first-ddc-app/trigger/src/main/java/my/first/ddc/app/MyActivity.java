df5f5eebcd056b2796a70921f36b52d0a755f815
Dmitry Gorohov <dmitry.gorohov@ebuilder.com>
2018-02-22 14:13:08 +0100

    Samples updated: trigger and scheduler parts added

--- android/first-ddc-app/trigger/src/main/java/my/first/ddc/app/MyActivity.java
+++ android/first-ddc-app/trigger/src/main/java/my/first/ddc/app/MyActivity.java
@@ -9,8 +9,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.stetho.Stetho;
 
 import io.ebuilder.mobile.services.DeviceDataCollectorFactory;
+import io.ebuilder.mobile.services.scheduler.ServiceTrigger;
 import io.ebuilder.mobile.services.settings.DeviceIdType;
 import io.ebuilder.mobile.services.utils.PermissionUtils;
 
@@ -24,9 +29,13 @@ public class MyActivity extends Activity {
     private final static int REQUEST_CODE = 100;
     private final static String SYSTEM_ID = "MySystemId";
 
+    private ServiceTrigger serviceTrigger;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Stetho.initializeWithDefaults(this);
+
         setContentView(R.layout.activity_main);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -35,27 +44,43 @@ public class MyActivity extends Activity {
                             Manifest.permission.READ_PHONE_STATE,
                             Manifest.permission.GET_ACCOUNTS
                     }, REQUEST_CODE);
-        } else {
-            setup();
         }
+        final Button cancelBtn = findViewById(R.id.cancel_btn);
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (serviceTrigger != null) {
+                    serviceTrigger.run(getApplicationContext());
+                }
+            }
+        });
     }
 
     private void setup() {
+        serviceTrigger = requestTrigger();
+    }
+
+    @SuppressLint("MissingPermission")
+    private ServiceTrigger requestTrigger() {
         if (PermissionUtils.allGranted(this,
                 Manifest.permission.READ_PHONE_STATE,
                 Manifest.permission.GET_ACCOUNTS)) {
             final TelephonyManager telephonyManager = (TelephonyManager)
                     getSystemService(TELEPHONY_SERVICE);
             if (telephonyManager != null) {
-                final String imei = telephonyManager.getDeviceId();
-                DeviceDataCollectorFactory.setup(getApplicationContext(),
-                        SYSTEM_ID, imei, DeviceIdType.IMEI)
+                return DeviceDataCollectorFactory.setup(
+                        SYSTEM_ID,
+                        telephonyManager.getDeviceId(),
+                        DeviceIdType.IMEI
+                )
                         .loggingEnabled()
                         .collectGoogleAccounts()
-                        .build(getApplicationContext());
+                        .build(this);
             }
         }
+        return null;
     }
+
 
     @SuppressLint("HardwareIds")
     @Override
